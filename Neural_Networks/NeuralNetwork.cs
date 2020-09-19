e8afb393b3e08df1920e0ac2623debce35385500
Yersen <44286140+Yersen@users.noreply.github.com>
2020-09-19 15:35:20 +0600

    Метод нормализации и масштабирования.

--- Neural_Networks/NeuralNetwork.cs
+++ Neural_Networks/NeuralNetwork.cs
@@ -34,21 +34,94 @@ namespace Neural_Networks
                 }
         }
 
-        public double Learn(List<Tuple<double,double[]>> dataSet, int epoch)//количество эпох обучения -epoch
+        public double Learn(double [] expected, double[,] inputs, int epoch)//количество эпох обучения -epoch
         {
             var error = 0.0;
-            for (int i = 0; i < epoch; i++)
+            for (int i = 0; i < expected.Length; i++)
             {
-                foreach (var data in dataSet)
+                for (int j = 0; j < expected.Length; j++)
                 {
-                    error += BackPropagation(data.Item1, data.Item2);
+                    var output = expected[j];
+                    var input = GetRow(inputs, j);
+
+                    error += BackPropagation(output, input);
                 }
             }
             var result = error / epoch;
             return result;
         }
 
-        private double BackPropagation(double expected, params double[] inputs)//обратное распространие
+        public static double[] GetRow(double[,] matrix, int row)
+        {
+            var columns = matrix.GetLength(1);
+            var array = new double[columns];
+            for (int i = 0; i < columns; i++)
+            {
+                array[i] = matrix[row, i];
+            }
+            return array;
+        }
+
+        private double[,] Scaling(double [,] inputs)//алгоритм масштабирования
+        {
+            var result = new double[inputs.GetLength(0), inputs.GetLength(1)];
+            for (int column = 0; column < inputs.GetLength(1); column++)
+            {
+                var min = inputs[0, column];
+                var max = inputs[0, column];
+
+                for (int row = 1; row < inputs.GetLength(0); row++)
+                {
+                    var item = inputs[row, column];
+
+                    if (item<min)
+                    {
+                        min = item;
+                    }
+                    if (item > max)
+                    {
+                        max = item;
+                    }
+                }
+                var divider = max - min;
+                for (int row = 1; row < inputs.GetLength(0); row++)
+                {
+                    result[row, column] = (inputs[row, column] - min) / divider;
+                }
+            }
+            return result;
+        }
+
+        private double[,] Normalization(double[,] inputs)//алгоритм нормализации
+        {
+            var result = new double[inputs.GetLength(0), inputs.GetLength(1)];
+            for (int column = 0; column < inputs.GetLength(1); column++)
+            {
+                //среднее значение сигнала
+                var sum = 0.0;
+                for (int row = 0; row < inputs.GetLength(0); row++)
+                {
+                    sum += inputs[row, column];
+                }
+                var average = sum / inputs.GetLength(0);
+
+                //стандартное квадратичное отклонение нейрона
+                var error = 0.0;
+                for (int row = 0; row < inputs.GetLength(0); row++)
+                {
+                    error += Math.Pow(inputs[row, column] - average, 2);
+                }
+                var standartError = Math.Sqrt(error / inputs.GetLength(0));
+
+                for (int row = 0; row < inputs.GetLength(0); row++)
+                {
+                    result[row, column] = (inputs[row, column] - average) / standartError;
+                }
+            }
+            return result;
+        }
+
+            private double BackPropagation(double expected, params double[] inputs)//обратное распространие
         {
             var actual = FeedForward(inputs).Output;
             var difference = actual - expected;
