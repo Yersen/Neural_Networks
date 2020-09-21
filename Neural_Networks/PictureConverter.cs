50ba93be399e834bdad6e1c6660b65706d405c4e
Yersen <44286140+Yersen@users.noreply.github.com>
2020-09-21 13:14:26 +0600

    Реализовано преобразование изображения в черно-белый формат

---
+++ Neural_Networks/PictureConverter.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+namespace Neural_Networks
+{
+    public class PictureConverter
+    {
+        public int Boundary { get; set; } = 128;
+        public int Height { get; set; }
+        public int Width { get; set; }
+        public List<int> Convert(string path)
+        {
+            var result = new List<int>();
+            Bitmap image = new Bitmap(path);//Bitmap — объект, используемый для работы с изображениями, определяемыми данными пикселей.
+            Height = image.Height;
+            Width = image.Width;
+            for (int y = 0; y < image.Height; y++)
+            {
+                for (int x = 0; x < image.Width; x++)
+                {
+                    var pixel = image.GetPixel(x, y);//получение пикселя
+                    var value = Brigtness(pixel);
+                    result.Add(value);
+                }
+            }
+            return result;
+        }
+
+        private int Brigtness(Color pixel)//яркость
+        {
+            var result = 0.299 * pixel.R + 0.587 * pixel.G + 0.114 * pixel.B;
+            return result < Boundary ? 0 : 1;
+        }
+        
+        public void Save(string path, List<int> pixels)
+        {
+            var image = new Bitmap(Width, Height);
+            for (int y = 0; y < image.Height; y++)
+            {
+                for (int x = 0; x < image.Width; x++)
+                {
+                    var color = pixels[y * Width + x] == 1 ? Color.White : Color.Black;
+                    image.SetPixel(x, y, color);
+                }
+            }
+            image.Save(path);
+        }
+    }
+}
---
+++ Neural_NetworksTests/PictureConverterTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Neural_Networks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neural_Networks.Tests
+{
+    [TestClass()]
+    public class PictureConverterTests
+    {
+        [TestMethod()]
+        public void ConvertTest()
+        {
+            var converter = new PictureConverter();
+            var inputs = converter.Convert(@"C:\Users\aser\source\repos\Neural_Networks\Neural_NetworksTests\images\Parasitized.png");
+            converter.Save(@"D:\image.png", inputs);
+        }
+    }
+}
